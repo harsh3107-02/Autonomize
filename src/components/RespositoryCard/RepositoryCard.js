46a67db1be31b171e0c40341cf9dc8c22121cdf1
harsh3107-02 <harshagrawal310702@gmail.com>
2024-12-29 12:30:25 +0530

    Add RepositoryCard component to display repository info

--- src/components/RespositoryCard/RepositoryCard.js
+++ src/components/RespositoryCard/RepositoryCard.js
@@ -1,41 +1,41 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import './RepositroyCard.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './RepositroyCard.css'
 
-// const RepositoryCard = ({ repo, username }) => {
-//   const truncatedTitle = repo.name.length > 15 ? repo.name.slice(0, 12) + '...' : repo.name;
+const RepositoryCard = ({ repo, username }) => {
+  const truncatedTitle = repo.name.length > 15 ? repo.name.slice(0, 12) + '...' : repo.name;
 
-//   return (
-//     <div className="repository-card">
-//       <div className="repository-card-content">
-//         <div className="repository-image">
-//           <img
-//             src={`https://picsum.photos/100?random=${Math.floor(Math.random() * 1000)}`} // Random image
-//             alt="Repository"
-//           />
-//         </div>
+  return (
+    <div className="repository-card">
+      <div className="repository-card-content">
+        <div className="repository-image">
+          <img
+            src={`https://picsum.photos/100?random=${Math.floor(Math.random() * 1000)}`} // Random image
+            alt="Repository"
+          />
+        </div>
 
-//         <div className="repository-details">-
-//           <Link to={`/repos/${username}/${repo.name}`}>
-//             <h3 style={{textDecoration:'none'}}>{truncatedTitle}<span
-//                 style={{
-//                   border: '2px solid #00c853',
-//                   backgroundColor: '#00c853',
-//                   borderRadius: '50%',
-//                   marginLeft: '5px',
-//                   marginTop: '10px',
-//                   fontSize: '10px',
-//                   color: '#00c853',
-//                 }}
-//               >
-//                 ✅
-//               </span></h3>
-//           </Link>
-//           <p>{repo.description || 'No description available.'}</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+        <div className="repository-details">-
+          <Link to={`/repos/${username}/${repo.name}`}>
+            <h3 style={{textDecoration:'none'}}>{truncatedTitle}<span
+                style={{
+                  border: '2px solid #00c853',
+                  backgroundColor: '#00c853',
+                  borderRadius: '50%',
+                  marginLeft: '5px',
+                  marginTop: '10px',
+                  fontSize: '10px',
+                  color: '#00c853',
+                }}
+              >
+                ✅
+              </span></h3>
+          </Link>
+          <p>{repo.description || 'No description available.'}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default RepositoryCard;
+export default RepositoryCard;
