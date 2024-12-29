da0706722d49c8c7971d4b51850322b1da97f49d
harsh3107-02 <harshagrawal310702@gmail.com>
2024-12-29 12:34:12 +0530

    add github servieces

--- src/services/githubService.js
+++ src/services/githubService.js
@@ -1,11 +1,11 @@
-// export const fetchUserData = (username) => {
-//     return Promise.all([
-//       fetch(`https://api.github.com/users/${username}`),
-//       fetch(`https://api.github.com/users/${username}/repos`),
-//     ]);
-//   };
+export const fetchUserData = (username) => {
+    return Promise.all([
+      fetch(`https://api.github.com/users/${username}`),
+      fetch(`https://api.github.com/users/${username}/repos`),
+    ]);
+  };
   
-//   export const fetchFollowers = (username) => {
-//     return fetch(`https://api.github.com/users/${username}/followers`);
-//   };
+  export const fetchFollowers = (username) => {
+    return fetch(`https://api.github.com/users/${username}/followers`);
+  };
   
